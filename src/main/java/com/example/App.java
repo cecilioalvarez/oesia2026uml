74fbbf995c61fee95a20edcae7fae77cec7a383e
cecilio <cmac@arquitecturajava.com>
2026-06-10 08:28:49 +0200

    inicio

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo!");
+        imprimirHolaMundo2();
+    }
+
+    public static void imprimirHolaMundo2() {
+        System.out.println("¡Hola Mundo 2!");
+    }
+}
